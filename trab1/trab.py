c83bbbab9ce0027d9198234fbab866480958707f
matheussn <matheusneto23@gmail.com>
2018-10-02 15:12:27 -0300

    Recebendo argumentos pelo terminal

--- trab1/trab.py
+++ trab1/trab.py
@@ -1,1 +1,11 @@
+# Alunos:
+# Mateus Benedini de Oliveira Prates - 
+# Matheus Santiago Neto - 11621BSI252
 
+import sys
+
+if __name__ == '__main__':
+	if len(sys.argv) > 2 or len(sys.argv) == 1:
+		exit()
+
+	base = sys.argv[1]
