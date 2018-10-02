9facbdd398069ce8aeead1df59fd0486045bf0f0
matheussn <matheusneto23@gmail.com>
2018-10-02 20:11:35 -0300

    Criando funções basicas

--- trab1/trab.py
+++ trab1/trab.py
@@ -1,20 +1,56 @@
 # Alunos:
-# Mateus Benedini de Oliveira Prates - 
+# Mateus Benedini de Oliveira Santiago Prates - 
 # Matheus Santiago Neto - 11621BSI252
 
 import sys
+from nltk.corpus import stopwords
 
-if __name__ == '__main__':
+# {'amor': {1: 3, 2:0}, ''}
+
+files = []
+dic = {}
+
+def init():
 	if len(sys.argv) > 2 or len(sys.argv) == 1:
 		exit()
 
-	files = []
+def initFiles():
 	base = open(sys.argv[1], 'r')
 	
-	for linha in base:
-		linha = linha.split()
-		files.append(open(linha[0], 'r'))
+	qnt = base.readlines()
 
-	print(files)
-	
+	for linha in qnt:
+		linha = linha.replace('\n', '')
+		files.append(open(linha, 'r'))
+
 	base.close()
+
+def c(str):
+	str = str.replace(',', ' ')
+	str = str.replace('.', ' ')
+	str = str.replace('!', ' ')
+	str = str.replace('?', ' ')
+	str = str.replace('\n', ' ')
+	return str
+
+#Retorna lista de palvras não stopwords
+def filtro(num):
+	sw = stopwords.words("portuguese")
+	conteudo = files[num].read()
+	conteudo = conteudo.lower()
+	conteudo = c(conteudo)
+	conteudo = conteudo.split()
+	
+	result = [rr for rr in conteudo if rr not in sw]
+
+	return result
+
+
+if __name__ == '__main__':
+	init()
+
+	initFiles()
+
+	for i in range(0, len(files)):
+		v = filtro(i)
+		print(v)
