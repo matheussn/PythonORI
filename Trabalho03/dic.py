a656fda8cdf0a8cf8b0405ad4e6b2405d5d32b90
matheussn <matheusneto23@gmail.com>
2018-11-30 00:04:37 -0200

    iniciando o terceiro trabalho

---
+++ Trabalho03/dic.py
@@ -0,0 +1,8 @@
+class Dicionarios:
+  def __init__(self):
+    self.index = {}
+    self.fileWeight = {}
+    self.queryWeight = {}
+    self.answer = {}
+    
+  
---
+++ Trabalho03/main.py
@@ -0,0 +1,5 @@
+from dic import Dicionarios
+
+if __name__ == '__main__':
+  c = Dicionarios()
+  print(str(c.index))
