954d61a0d928daee13a14168fc96aa096cbccd28
mateusbprates <mateusbprates@hotmail.com>
2018-12-09 17:43:30 -0200

    dicQueries

--- Trabalho03/func.py
+++ Trabalho03/func.py
@@ -3,6 +3,6 @@ import math
 def tf(freq):
 	return ( 1+ math.log(freq,10) ) 
 	
-def idf(TotalDocumentos,nDocumentosComTermo)
+def idf(TotalDocumentos,nDocumentosComTermo):
 	aux = TotalDocumentos/nDocumentosComTermo
 	return math.log(aux,10)
